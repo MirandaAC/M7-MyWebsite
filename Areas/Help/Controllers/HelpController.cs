9cd0fac2218f47aae3d663de9b4cb4dfa4580ac4
Mo <Mo@DESKTOP-PBUN1VI>
2024-03-09 15:15:08 -0500

    Add project files.

---
+++ Areas/Help/Controllers/HelpController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebsite.Areas.Help.Controllers
+{
+    [Area("Help")]
+    public class HelpController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using MyWebsite.Models;
+using System.Diagnostics;
+
+namespace MyWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Help() {
+            return View("./Areas/Help/Views/Home/Index.cshtml");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
